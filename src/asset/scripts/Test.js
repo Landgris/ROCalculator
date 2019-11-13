509f97e8c23c29e818689f5c338141367bf4fe69
Landgris <valkyrie7744533@gmail.com>
2019-11-13 20:31:22 +0800

    js test

---
+++ src/asset/scripts/Test.js
@@ -0,0 +1,31 @@
+
+var vm = new Vue({
+    el:'#app',
+    data:{
+        message: "local data",
+    },
+    methods:{
+        getRemoteMessage()
+        {
+            Promise.resolve("Get Remote Data")
+            .then((res)=>{
+                this.message = res;
+            });
+        }
+    }
+});
+
+new Vue({
+    el: '#app2',
+    data: function() 
+    {
+        return { visible: false }
+    }
+})
+
+function clickButton() {    // 2. trigger event in Vue instance
+    Promise.resolve("Get remote data by outside button.") // 3. Get data asynchronously
+        .then((res) => {    // 4. Return result
+            vm.message = res; // 5. Change View Model
+        });
+}
